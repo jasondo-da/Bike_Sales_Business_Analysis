984c3f1e4b329fa21d2f3c862a1bc39be02cf004
Jason Do <138195365+jasondo-da@users.noreply.github.com>
2025-04-13 23:04:26 -0700

    Create bike_sales_queries.sql
    analysis outline

---
+++ bike_sales_queries.sql
@@ -0,0 +1,30 @@
+---------------
+-- Query SQL --
+---------------
+
+-- Geographic Performance --
+/* What are the total historical sales by state? */
+
+/* 	Which areas contain the largest addressable market? */
+
+/* 	Which cities generate the most revenue? */
+
+/* 	Which stores are historical sales leaders and losers? */
+
+-- Product Performance --
+/* 	Which category are the best sellers? */
+
+/* 	Which sellers generate the most sales? */
+
+/* 	Which items are the best sellers? */
+
+/* 	Which products are the most profitable? */
+
+/* 	Do discounts affect total sales? */
+
+-- Seasonal Performance --
+/* 	Which months historically move the most volume?  Most Sales? */
+
+/* 	Which month is historically the most profitable month? */
+
+/* 	Does the shipping time affect sales? */
