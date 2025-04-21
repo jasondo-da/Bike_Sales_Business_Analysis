c23bd99ab546bf97e14210bdde7fee901d548e3d
Jason Do <138195365+jasondo-da@users.noreply.github.com>
2025-04-20 23:34:29 -0700

    Update bike_sales_queries.sql
    updated code

--- bike_sales_queries.sql
+++ bike_sales_queries.sql
@@ -60,6 +60,10 @@ LEFT JOIN order_items ot
 GROUP BY s.store_id, s.store_name, s.state
 ORDER BY total_sales DESC
 
+
+
+
+
 -- Product Performance --
 
 /* 	Which brands generate the most sales? */
@@ -89,6 +93,7 @@ ORDER BY total_sales DESC
 
 /* 	Which items are the best sellers? */
 SELECT
+	p.product_id,
 	p.product_name,
     ROUND((SUM(ot.quantity * ot.list_price) - SUM(ot.discount)), 2) total_sales
 FROM order_items ot
@@ -96,7 +101,7 @@ LEFT JOIN products p
 	ON ot.product_id = p.product_id
 LEFT JOIN brands b
 	ON p.brand_id = b.brand_id
-GROUP BY p.product_name
+GROUP BY p.product_id, p.product_name
 ORDER BY total_sales DESC
 LIMIT 10
 
@@ -107,10 +112,56 @@ SELECT
 	1 - ((SUM(ot.quantity * ot.list_price) - SUM(ot.discount))) / (SUM(ot.quantity * ot.list_price)) percentage_of_sales
 FROM order_items ot
 
+
+
+
+
+
 -- Seasonal Performance --
 
 /* 	Which months historically move the most volume?  Most Sales? */
-
-/* 	Which month is historically the most profitable month? */
+SELECT 
+	MONTH(o.order_date),
+    COUNT(o.order_id) total_orders,
+    ROUND((SUM(ot.quantity * ot.list_price) - SUM(ot.discount)), 2) net_sales,
+    ROUND(((SUM(ot.quantity * ot.list_price) - SUM(ot.discount)) / COUNT(o.order_id)), 2) avg_order_total
+FROM orders o
+LEFT JOIN order_items ot
+	ON o.order_id = ot.order_id
+GROUP BY MONTH(o.order_date)
 
 /* 	Does the shipping time affect sales? */
+WITH late_orders AS (
+	SELECT 
+		COUNT(o.order_id) total_late_orders,
+		ROUND(((SUM(ot.quantity * ot.list_price) - SUM(ot.discount)) / (COUNT(o.order_id))), 2) avg_total_per_late_order
+	FROM orders o
+	LEFT JOIN order_items ot
+		ON o.order_id = ot.order_id
+	WHERE o.shipped_date > o.required_date),
+total_orders AS (
+	SELECT 
+		COUNT(o.order_id) total_orders,
+		ROUND(((SUM(ot.quantity * ot.list_price) - SUM(ot.discount)) / (COUNT(o.order_id))), 2) avg_total_per_order
+	FROM orders o
+	LEFT JOIN order_items ot
+		ON o.order_id = ot.order_id)
+
+SELECT 
+	total_orders,
+    avg_total_per_order,
+    total_late_orders,
+    avg_total_per_late_order,
+    ROUND(((total_late_orders / total_orders) * 100), 2) percentage_late_orders,
+    ROUND((avg_total_per_order - avg_total_per_late_order), 2) net_sales_change
+FROM total_orders, late_orders
+
+/* Which stores have the most rejected sales? */
+SELECT 
+	s.store_id,
+	COUNT(*)
+FROM orders o
+LEFT JOIN stores s
+	ON o.store_id = s.store_id
+WHERE o.order_status = 3
+GROUP BY s.store_id
